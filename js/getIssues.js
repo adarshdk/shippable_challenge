5aea922b01894d77c6acb65392629cc71975412a
Adarsh Kyadige <Adarsh Kyadige>
2017-02-23 19:40:06 -0800

    Add comments to js

--- js/getIssues.js
+++ js/getIssues.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
-
+	//Define behaviour on click of the Refresh open issues button
 	$("#refresh").click(function(){
+		//Get repo clone link, and parse it to get the repo name and owner
 		var repoLink = $("input#repo_name").val();
 		var repoArray = repoLink.split("/");
 		var repoName = repoArray[repoArray.length-1].split(".")[0];
 		var repoOwner = repoArray[repoArray.length-2];
+		//Use jQuery's getJSON method to get information about open issues on a repo.
+		//Then, add the information obtained using a callback to a table on the page.
 		$.getJSON( "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/issues", function( data ) {
 			var	tableString = "<tr><td>Issue Number</td><td>Title</td><td>User</td><td>Created Time</td></tr>";
 			for(var i = 0; i< data.length; i++){
